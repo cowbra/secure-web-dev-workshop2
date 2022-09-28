f6cdad3037d7e2021e2b296fd9c8fb6f6ba55355
cowbra <hugosanchez1303@gmail.com>
2022-09-28 22:07:15 +0200

    update import js
    update import data .js to fill the dbb with filmcollection

--- import-data.js
+++ import-data.js
@@ -1,9 +1,8 @@
 //https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
 const  mongoose = require('mongoose')
-
 require('dotenv').config()
 
-
+const filmingLocations = require('./lieux-de-tournage-a-paris.json')
 
 //import mongoose from 'mongoose';
 const { Schema } = mongoose;
@@ -21,7 +20,7 @@ const Locations = new Schema({
             },
             type: {
                 type: String,
-                enum:["Point"]
+                enum:["Point",""]
             }
         },
     sourceLocationId: String,
@@ -37,8 +36,36 @@ module.exports = LocationModel;
 
 
 
-function importFilmingLocationInMongo(){
-    for(let)
+async function importFilmingLocationInMongo(){
+    let chunk = []
+    for (const film of filmingLocations) {
+        const newFilmModelToInsert = new LocationModel({ filmType: film.fields.type_tournage},
+            { filmProducerName: film.fields.nom_producteur },
+            { endDate: film.fields.date_fin},
+            { filmName: film.fields.nom_tournage},
+            { district: film.fields.ardt_lieu},
+            { geolocation: film.fields.geo_shape.coordinates },
+            { type: film.fields.geo_shape.type },
+            { sourceLocationId: film.fields.id_lieu },
+            { filmDirectorName: film.fields.nom_realisateur},
+            { adress: film.fields.adresse_lieu},
+            { startDate: film.fields.date_debut},
+            { year: film.fields.annee_tournage });
+        //On insere des promesses de donnees à envoyer à la bdd
+        chunk.push(newFilmModelToInsert.save())
+        
+        //tous les 100 models de films, on envoie à la bdd mongo
+        if(chunk.length===100){
+            await Promise.all(chunk)
+            chunk = []
+        }
+        //on envoie les models restants
+        const results = await Promise.all(chunk)
+        //on affiche le resultat de la promesse des derniers elements
+        console.log(results)
+
+
+    }
 
 }
 
@@ -46,14 +73,11 @@ async function main(){
     const connected = await mongoose.connect(process.env.MONGO_URI);
     console.log(connected);
 
-    const horrorMovie = new LocationModel({filmType:'horror'});
-    await horrorMovie.save();
+    //On envoie les models sur mongo
+    importFilmingLocationInMongo()
     await mongoose.connection.close();
 }
 
 
 
-main()
-
-/*
-new Model({fgfg})
+main();
